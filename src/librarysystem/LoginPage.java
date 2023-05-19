91a048e0b9e67b990c648f04a3ad21c05ca7e8e2
Hidayet <hidayetachak@gmail.com>
2023-05-19 22:20:16 +0500

    login page

---
+++ src/librarysystem/LoginPage.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package librarysystem;
+
+/**
+ *
+ * @author Hidayet
+ */
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginPage {
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+
+    public LoginPage() {
+        JFrame frame = new JFrame("Login Page");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+
+        JPanel panel = new JPanel();
+
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameField = new JTextField(20);
+
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordField = new JPasswordField(20);
+
+        JButton loginButton = new JButton("Login");
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String password = new String(passwordField.getPassword());
+
+                if (username.equals("admin") && password.equals("password")) {
+                    JOptionPane.showMessageDialog(frame, "Login successful!");
+                    // if login success show admin page
+                } else {
+                    JOptionPane.showMessageDialog(frame, "Invalid username or password. Please try again.");
+                }
+            }
+        });
+
+        panel.add(usernameLabel);
+        panel.add(usernameField);
+        panel.add(passwordLabel);
+        panel.add(passwordField);
+        panel.add(loginButton);
+
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        LoginPage loginPage = new LoginPage();
+    }
+}
